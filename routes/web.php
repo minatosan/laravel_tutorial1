458eae053a49b94c0efbf0823d05bc0613024173
minato_offgrid <y.kobayashi@offgrid.co.jp>
2022-02-02 19:31:05 +0900

    フォームを追加

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/folders/{id}/tasks','App\Http\Controllers\TaskController@index')->name('tasks.index');
+Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
+Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
 Route::get('/', function () {
     return view('welcome');
 });
