6dccdc812797ca057fae34f644feb2f77c24432d
minato_offgrid <y.kobayashi@offgrid.co.jp>
2022-02-02 17:43:19 +0900

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Folder;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function index(int $id)
+    {
+        $folders = Folder::all();
+
+        // 選ばれたフォルダを取得する
+        $current_folder = Folder::find($id);
+
+        // 選ばれたフォルダに紐づくタスクを取得する
+        $tasks = Task::where('folder_id', $current_folder->id)->get();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
+        ]);
+    }
+}
