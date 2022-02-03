473c1a1f4d1a5511e50749c35a3a907b8443e07c
minato_offgrid <y.kobayashi@offgrid.co.jp>
2022-02-03 10:51:21 +0900

    バリデーションの実装＆yield構造の施工＆testの作成

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Folder;
+use App\Http\Requests\CreateFolder; 
 
 class FolderController extends Controller
 {
@@ -11,7 +12,7 @@ class FolderController extends Controller
     {
         return view('folders/create');
     }
-    public function create(Request $request)
+    public function create(CreateFolder  $request)
    {
     // フォルダモデルのインスタンスを作成する
     $folder = new Folder();
